4c4e44af9572279cd3a8f2c4ca7d59004a8d0ea9
Suzana <suzanasp1026@gmail.com>
2023-05-23 20:54:06 -0600

    some changes

--- new_agent.py
+++ new_agent.py
@@ -36,22 +36,22 @@ class Agent:
         if isinstance(self.state, DancingState) and self.state.dances >= 3:
             self.state = RestingState()
             
-    def bee_movement(self):
+ 
+
+world_model.x += Agent.x
+world_model.y += Agent.y
+
+environment = world_model.environment
+
+def update():
+    world_model.x[-1] += 0.5
+    world_model.y[-1] += 0.5
+    environment.set_offsets(world_model.x, world_model.y)
     
-        world_model.x += self.x
-        world_model.y += self.y
+ani = animation.FuncAnimation(environment, update, frames=100, interval=100)
 
-        environment = world_model.environment
+plt.show()
 
-        def update():
-            world_model.x[-1] += 0.5
-            world_model.y[-1] += 0.5
-            environment.set_offsets(world_model.x, world_model.y)
-            
-        ani = animation.FuncAnimation(environment, update, frames=100, interval=100)
-
-        plt.show()
-        
     
         
             
